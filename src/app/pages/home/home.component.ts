d0bb23927aa36d5acb9195fa27b60c160314b438
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2021-02-07 13:23:11 +0200

    about info to home page added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,14 +27,17 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  // slides = [
-  //   { img: "http://placehold.it/350x150/000000" },
-  //   { img: "http://placehold.it/350x150/111111" },
-  //   { img: "http://placehold.it/350x150/333333" },
-  //   { img: "http://placehold.it/350x150/666666" }
-  // ];
-
-  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1, "autoplay": true, "autoplaySpeed": 3000, "fade": true, "appendDots": true };
+  slideConfig = {
+    "slidesToShow": 1, "slidesToScroll": 1, "autoplay": true, "autoplaySpeed": 1200, "fade": true,
+    "responsive": [
+      {
+        "breakpoint": 767,
+        "settings": {
+          "appendDots": "#start-screen__slider-nav"
+        }
+      }
+    ]
+  };
 
   slickInit(e) {
     console.log('slick initialized');
