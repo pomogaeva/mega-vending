af17b21e6acc211c46623efaa389c8600db548c6
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2021-02-14 19:33:14 +0200

    added product categories to home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HomeSlide } from 'src/app/interfaces/home-slide';
+import { ProductCategory } from 'src/app/interfaces/product-category';
 import { HomeSlidesService } from 'src/app/services/home-slides.service';
+import { ProductCategoryService } from 'src/app/services/product-category.service';
 declare const VendiGO: any;
 
 @Component({
@@ -15,14 +18,20 @@ export class HomeComponent implements OnInit {
   homeSlideData: HomeSlide;
   homeSlidesList: Array<HomeSlide>;
 
+  productCategoryData: ProductCategory;
+  productCategoryList: Array<ProductCategory>;
+
   constructor(
-    public homeSlidesService: HomeSlidesService
+    public homeSlidesService: HomeSlidesService,
+    private route: ActivatedRoute,
+    public productCategoryService: ProductCategoryService
   ) {
   }
 
   ngOnInit(): void {
     VendiGO.onReady();
     this.getHomeSlides();
+    this.getProductCategoryData();
   }
 
   getHomeSlides(): void {
@@ -49,6 +58,12 @@ export class HomeComponent implements OnInit {
 
   breakpoint(e) {
     console.log('breakpoint');
+  }
+
+  getProductCategoryData(): void {
+    this.productCategoryService.productCategoryData.subscribe(data => {
+      this.productCategoryList = data;
+    })
   }
 
 }
